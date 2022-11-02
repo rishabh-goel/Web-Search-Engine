cd41f3aa64c1329b2e69adfc1024d68cb2542aa0
rishabhgoel <goel.rishabh12@gmaill.com>
2022-11-02 14:54:46 -0500

    Updated to avoid errors

--- WebCrawler.py
+++ WebCrawler.py
@@ -5,6 +5,8 @@ from urllib.parse import urljoin, urlparse
 
 from AtomicCounter import Counter
 from WebScraper import scrape_page, scrape_links, scrape_info
+import sys
+sys.setrecursionlimit(10000)
 
 '''
 
@@ -14,7 +16,8 @@ multi-threaded crawle simultaneously 10 worker threads scrape the UIC domain
 '''
 
 
-# Challenges - multithreading webscraping, redirected sites, .shtml pages
+# Challenges - multithreading webscraping, redirected sites, .shtml, aspx pages,
+# RecursionError: maximum recursion depth exceeded while calling a Python object
 class WebCrawler:
 
     def __init__(self, base_url):
@@ -24,8 +27,8 @@ class WebCrawler:
         self.pool = ThreadPoolExecutor(max_workers=10)
         self.domain = 'uic.edu'
         self.atomicCounter = Counter(0)
-        self.invalid_extensions = ["pdf", "jpg", "jpeg", "doc", "docx", "ppt", "pptx", "png", "txt", "exe", "ps", "psb",
-                                   "shtml", "aspx"]
+        self.invalid_extensions = [".pdf", ".jpg", ".jpeg", ".doc", ".docx", ".ppt", ".pptx", ".png", ".txt", ".exe",
+                                   ".ps", ".psb", ".shtml", ".aspx", "mailto:", ".html", ".php", ".asp"]
         # All the scraped pages
         self.crawled_pages = set([])
         self.dictionary = {}
@@ -46,26 +49,31 @@ class WebCrawler:
         links = scrape_links(html)
         for link in links:
             url = link['href']
+            url = url.strip()
             if (url.startswith('/') or self.domain in url) and ('.com' not in url):  # run only in the uic.edu domain
                 url = urljoin(self.root_url, url)
                 if url not in self.crawled_pages and '@' not in url and self.is_valid_extension(url):
                     if not url.endswith("/"):
                         url = url + "/"  # appending / in the end to avoid duplicate runs
-                    if "https" not in url:
-                        url = url.replace("http", "https")
+                    if "https" in url:
+                        url = url.replace("https", "http")
                     self.to_crawl.put(url)
 
     def post_scrape_callback(self, res):
         if res is not None:
             result = res.result()
-            if result[0] and result[0].status_code == 200:
-                self.parse_links(result[0].text)
-                scrape_info(result[0].text, result[2])
+            # if result[0].status_code == 200 and result[0]:
+            if result and result[0]:
+                if result[0].status_code == 200:
+                    self.parse_links(result[0].text)
+                    scrape_info(result[0].text, result[2])
 
     def run_scraper(self, num_pages):
         while True:
             try:
                 target_url = self.to_crawl.get(timeout=120)  # wait for 120 sec for a page to add a url to be parsed
+                if "https" in target_url:
+                    target_url = target_url.replace("https", "http")
                 if target_url not in self.crawled_pages:
                     self.crawled_pages.add(target_url)
                     if len(self.crawled_pages) <= num_pages:
@@ -78,7 +86,10 @@ class WebCrawler:
             except Empty:
                 return
             except Exception as e:
-                print(e)
+                print(f'Exception is \n{e}')
+                # TODO: remove errored url
+                self.dictionary = {key: val for key, val in self.dictionary.items() if val != target_url}
+                self.crawled_pages.remove(target_url)
                 continue
 
     def store_crawled_pages(self):
