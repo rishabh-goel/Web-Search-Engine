d81c3cf70b1f642c4e95b678218a3d2a3868a0e1
rishabhgoel <goel.rishabh12@gmaill.com>
2022-11-07 12:26:00 -0600

    Test file to verify docs and url list

--- test.py
+++ test.py
@@ -16,22 +16,21 @@ def tag_visible(element):
 
 
 if __name__ == '__main__':
-    url = 'http://www.cs.uic.edu/Sistla/'
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-    res = requests.get(url, timeout=(3, 60), verify=False, headers=HEADERS)
-    print(res.url)
-    print(res.status_code)
-    print(res.text)
-
-    soup = BeautifulSoup(res.text, 'html.parser')
-    links = soup.find_all('a', href=True)
-    web_page_paragraph_contents = soup.find_all(text=True)
-    web_page_paragraph_contents = filter(tag_visible, web_page_paragraph_contents)
-    text = " ".join(term.strip() for term in web_page_paragraph_contents)
-    text = text.lower()
-
-    text = re.sub("[^a-z]+", " ", text)
-    # print(text)
+    # url = 'http://www.cs.uic.edu/Sistla/'
+    # HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+    # res = requests.get(url, timeout=(3, 60), verify=False, headers=HEADERS)
+    # print(res.url)
+    # print(res.status_code)
+    # print(res.text)
+    #
+    # soup = BeautifulSoup(res.text, 'html.parser')
+    # links = soup.find_all('a', href=True)
+    # web_page_paragraph_contents = soup.find_all(text=True)
+    # web_page_paragraph_contents = filter(tag_visible, web_page_paragraph_contents)
+    # text = " ".join(term.strip() for term in web_page_paragraph_contents)
+    # text = text.lower()
+    #
+    # text = re.sub("[^a-z]+", " ", text)
 
     list1 = []
     for filename in os.listdir('/Users/rishabhgoel/Documents/Fall22/IR/Web-Search-Engine/uic-docs-text'):
@@ -41,13 +40,10 @@ if __name__ == '__main__':
     with open("mapping.txt") as f:
         content = f.readlines()
 
-    # print(content)
     for line in content:
         line = line.strip()
-        # print(line)
         num = line.split()[0]
-        # print(num)
         list2.append(int(num))
 
-    main_list = list(set(list2).difference(list1))
-    print(sorted(main_list))
+    print(sorted(list(set(list2).difference(list1))))
+    print(sorted(list(set(list1).difference(list2))))
